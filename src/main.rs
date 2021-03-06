e871e10ef6218b2d2df739ff37d32d885d95b4bf
Andrew <Andrew5282011@gmail.com>
2021-03-06 16:02:14 +0300

    Implement guess-game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess (type \"quit\" to exit)");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        if guess.trim() == "quit" {
+            println!("Ok, bye!");
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Incorrect number! Please try again!");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! Try again!"),
+            Ordering::Greater => println!("Too big! Try again!"),
+            Ordering::Equal => {
+                println!("You did it! Congrats!");
+                break;
+            }
+        }
+    }
 }
